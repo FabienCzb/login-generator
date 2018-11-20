681ba47ee5fda635324e83d974347da5a9f6750c
FabienCzb <fabienc97@gmail.com>
2018-11-20 12:01:51 +0100

    Remplacement src et creation test LoginService #1

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,22 @@
+package geco;
+
+import static org.junit.Assert.*;
+
+public class LoginServiceTest {
+
+    @org.junit.Test
+    public void loginExists() {
+    }
+
+    @org.junit.Test
+    public void addLogin() {
+    }
+
+    @org.junit.Test
+    public void findAllLoginsStartingWith() {
+    }
+
+    @org.junit.Test
+    public void findAllLogins() {
+    }
+}
